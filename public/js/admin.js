83c575a3d4422b7868359554324233d9c8a83eed
mahdilao <mahdilao@qq.com>
2016-11-04 18:22:00 +0800

    添加豆瓣电影查询AIP，对首页样式进行局部调整

--- public/js/admin.js
+++ public/js/admin.js
@@ -17,4 +17,28 @@ $(function (){
 			}
 		})
 	})
+	// 调用豆瓣API
+	$('#douban').blur(function(){
+		var douban = $('#douban')
+		var id = douban.val()
+		if (id){
+			$.ajax({
+				url: 'https://api.douban.com/v2/movie/subject/' + id,
+				cache: true,
+				type: 'get',
+				dataType: 'jsonp',
+				crossDomain: true,  // 跨域请求
+				jsonp: 'callback',
+				success: function(data){
+					$('#inputTitle').val(data.title)
+					$('#inputDoctor').val(data.directors[0].name)
+					$('#inputCountry').val(data.countries[0])
+					$('#inputPoster').val(data.images.large)
+					$('#inputYear').val(data.year)
+					$('#inputSummary').val(data.summary)
+				}
+			})
+		}
+	})
+
 })
